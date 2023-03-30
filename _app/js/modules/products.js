1739016eeeea8d0560a1ac6a2c2044b5faf21541
Abdihamidroble20@hotmail.com <111975234+Abdi199@users.noreply.github.com>
2023-03-30 18:12:07 +0200

    added the right query

--- _app/js/modules/products.js
+++ _app/js/modules/products.js
@@ -1,7 +1,12 @@
 import {sanity} from '../sanity.js'
 
 export default async function products() {
-	const query = `*[_type == 'product']`;
+	const query = `*[_type == 'product'] {
+		name,	
+		price,
+		'imageUrl': image.asset->.url,
+		'brandName': brand->.name,
+	}`;
 
 	const products = await sanity.fetch(query);
 
